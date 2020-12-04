36deb8c8f1adc59afb84cfb8040697f863a64f57
benesjan <janbenes1234@gmail.com>
2020-12-04 15:31:32 -0600

    Updated api endpoint and CRON tasks

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask
 
 from src.balancer.balancer import Balancer
 from src.controller import Controller
+from src.shared.type_definitions import Exchange
+from src.uniswap_v2.uniswap import Uniswap
 from src.workarounds.uniswap_matching_txs import UniMatchingTxs
 
 app = Flask(__name__)
@@ -17,11 +19,15 @@ def update(exchange, entity_type, min_liquidity=None):
         # controller = Controller(Uniswap())
     elif exchange == 'BALANCER':
         controller = Controller(Balancer())
+    elif exchange == 'SUSHI':
+        controller = Controller(Uniswap(dex_graph_name='benesjan/sushi-swap', exchange=Exchange.SUSHI))
     else:
         return '{"success": false, "exception": "Unknown exhchange type."}'
     try:
         if entity_type == 'snaps':
             controller.update_snaps(max_objects_in_batch=100)
+        elif entity_type == 'staked_snaps':
+            controller.update_staked_snaps(max_objects_in_batch=100)
         elif entity_type == 'pools':
             if min_liquidity is None:
                 return '{"success": false, "exception": "None min_liquidity URL parameter in update of pools."}'
